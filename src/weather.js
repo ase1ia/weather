13718d58ecbf66cfa51230e1506a715996f1e585
ase1ia <56377522+ase1ia@users.noreply.github.com>
2020-03-12 08:53:38 +0600

    add s to http

--- src/weather.js
+++ src/weather.js
@@ -24,7 +24,7 @@ class Weather extends Component {
         this.setState({
             loader: true
         }, () => {
-            {fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`)
+            {fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`)
                     .then(res => res.json())
                     .then(data => {
                         if(data.cod === 200){
